96c97446ee46a58707f96b36a137625667d9987d
Vladislav <63506567+hjftyd@users.noreply.github.com>
2022-06-08 03:24:55 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let client = {
+    "ProfileId": "12w562qrx",
+    "PersonInfo": {
+        "Name": "Василий Иванов",
+        "BirthDate": "12-09-1990",
+        "Citizenship": "Russian Federation"
+    },
+    "CardInfo": [
+        {
+            "CardNumber": "1234890456793333",
+            "CardName": "VISA CLASSIC",
+            "ExpDate": "30-02-2019"
+        },
+        {
+            "CardNumber": "1234000145292133",
+            "CardName": "MASTERCARD GOLD",
+            "ExpDate": "21-05-2020"
+        },
+        {
+            "CardNumber": "1234000145293333",
+            "CardName": "MIR КЛАССИЧЕСКАЯ",
+            "ExpDate": "02-12-2019"
+        }
+    ]
+ }
+
+function getCardsArrayExpDate(json, date) {
+    let data = JSON.parse(json);
+    let cardArray = [];
+    for (let i = 0; i < data.CardInfo.length; i++) {
+       if (convertDate(data.CardInfo[i].ExpDate) < convertDate(date)) {
+            cardArray.push(data.CardInfo[i].CardNumber.substring(data.CardInfo[i].CardNumber.length - 4));
+        }
+    }
+    if (cardArray.length) {
+        return cardArray;
+    }
+    return null;
+}
+    const info = JSON.stringify(client)
+    let clientInfo = info
+    function convertDate(date) {
+        let part = date.split('-');
+        return new Date(part[2] + '-' + part[1] + '-' + part[0]);
+    }
+console.log(getCardsArrayExpDate(clientInfo, '21-05-2020'));
+
